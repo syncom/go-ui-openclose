bead29455f883b32648946d8e28d99643e02ed03
Ning Shang <syncom.dev@gmail.com>
2018-02-02 01:11:08 -0800

    Bug fix and some improvements.

--- uncloak.sh
+++ uncloak.sh
@@ -9,18 +9,21 @@ NVME_KO=/lib/modules/${KERNEL_VER}/kernel/drivers/nvme/host/nvme.ko
 NS1_MOUNT_POINT=/mnt/nvme
 NS2_MOUNT_POINT=/mnt/nvme2
 
+SHORTSLEEP=2
+LONGSLEEP=20
+
 COMMAND="nvme admin-passthru ${VISIBLE_DRIVE} -o 0xC1 -n 1 "\
 "--cdw11=0 --cdw10=8 -l 512 -i ${TOKEN_FILE} -w -s"
 
 eval ${COMMAND} > /dev/null 2>&1 || exit 1
-sleep 2
+sleep ${SHORTSLEEP}
 umount ${NS1_MOUNT_POINT} > /dev/null 2>&1
 umount ${NS2_MOUNT_POINT} > /dev/null 2>&1
-sleep 2
+sleep ${SHORTSLEEP}
 rmmod nvme > /dev/null 2>&1 || exit 3
-sleep 20
+sleep ${LONGSLEEP}
 insmod ${NVME_KO} > /dev/null 2>&1 || exit 4
-sleep 2
+sleep ${SHORTSLEEP}
 mount ${VISIBLE_DRIVE} ${NS1_MOUNT_POINT} > /dev/null 2>&1 || exit 5
 mount ${HIDDEN_DRIVE} ${NS2_MOUNT_POINT} > /dev/null 2>&1 || exit 6
 
